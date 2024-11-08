2667963a77c47f9502734cc3597d2bb31eae81f8
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-11-08 21:19:11 +0700

    chore: remove some trash

--- app/(tabs)/home-screen.tsx
+++ app/(tabs)/home-screen.tsx
@@ -1,14 +1,6 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { LandmarkViewCard } from '@/components/landmark';
-import { SpecialDishViewCard } from '@/components/dish/SpecialDishViewCard';
-import { UserReviewCard } from '@/components/user/UserReviewCard';
-import { HStack } from '@/components/ui/hstack';
-import { CategoryBadge } from '@/components/badge';
 import { DashboardHeader } from '../(dashboard)/components/DashboardHeader';
 import { SearchInput } from '../(dashboard)/components/SearchInput';
 
@@ -18,81 +10,7 @@ export default function HomeScreen() {
       header={<DashboardHeader />}
     >
       <SearchInput />
-      <LandmarkViewCard
-        landmark={{
-          id: '1',
-          name: 'Landmark Name',
-          image: 'https://images.unsplash.com/photo-1729180801690-d7db9ea35867?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          location: 'SpecialDish Location',
-          distance: 0.5,
-          rating: 4.5,
-        }}
-      />
-      <SpecialDishViewCard
-        specialDish={{
-          name: 'SpecialDish Name',
-          image: 'https://images.unsplash.com/photo-1729180801690-d7db9ea35867?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        }}
-      />
-      <UserReviewCard
-        review={{
-          user: {
-            name: 'Jane Doe',
-            image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-          },
-          vote: 4,
-          review: 'Great place to visit!',
-          created_at: '2021-10-10',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies aliquam. Nullam nec purus nec nunc ultricies aliquam.',
-        }}
-      />
-      <UserReviewCard
-        review={{
-          user: {
-            name: 'Jane Doe',
-            image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-          },
-          vote: 2,
-          review: 'Great place to visit!',
-          created_at: '2021-10-10',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies aliquam. Nullam nec purus nec nunc ultricies aliquam.',
-        }}
-      />
-      <HStack space='md'>
-        <CategoryBadge category='landmark' />
-        <CategoryBadge category='specialDish' />
-      </HStack>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+
     </ParallaxScrollView>
   );
 }
